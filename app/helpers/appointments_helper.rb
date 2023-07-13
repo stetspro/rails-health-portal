00c69a4f8e35c8aec29ab3ad944a28404e186a81
stetspro <stets.2033@ukr.net>
2023-07-13 17:34:30 +0000

    add 30 min time slots helper method for appointment

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -1,2 +1,13 @@
 module AppointmentsHelper
+  # generate an array of half-hour time slots that the user can choose from
+  def time_slots
+    start_time = Time.parse("9:00")
+    end_time = Time.parse("20:00")
+    time_slots = []
+    while start_time <= end_time
+      time_slots << start_time.strftime("%I:%M %p")
+      start_time += 30.minutes
+    end
+    time_slots
+  end
 end
