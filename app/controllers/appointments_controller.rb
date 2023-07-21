4b16f882e20087fb4de03b7ea002169451f31258
stetspro <stets.2033@ukr.net>
2023-07-21 14:06:11 +0000

    add comments to appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,44 +1,63 @@
 class AppointmentsController < ApplicationController
+   # Ensure the patient is logged in for all actions in this controller
   before_action :logged_in_patient
   before_action :set_patient, only: [:new, :create, :choose_time]
 
+  # Prepare for creating a new appointment
   def new
+    # Fetch the current logged-in patient
     @patient = current_patient
+    # Build a new blank appointment for the form
     @appointment = Appointment.new
+    # Fetch all doctors for the doctor selection dropdown
     @doctors = Doctor.all
+    # Retrieve any suggested date from the flash message
     @suggested_date = flash[:openai_suggested_date]
-    @all_times = (9..12).to_a + (14..20).to_a # all times from 9 to 20, excluding 13
+    # Define the range of possible appointment times, from 9 to 20, excluding 13
+    @all_times = (9..12).to_a + (14..20).to_a
   end
   
-  def create
+   # Process the form submission for creating a new appointment
+   def create
+    # Fetch the current logged-in patient
     @patient = current_patient
+    # Fetch the chosen doctor
     @doctor = Doctor.find(params[:appointment][:doctor_id])
+    # Check if a date was chosen for the appointment
     if params[:appointment][:appointment_date].present?
-      # Store the appointment_date in the session
+      # If a date was chosen, store it in the session
       session[:appointment_date] = params[:appointment][:appointment_date]
+      # Redirect to the time selection page
       redirect_to choose_time_patient_appointments_path(patient_id: @patient.id, doctor_id: @doctor.id)
     else
+      # If no date was chosen, display an error
       @doctors = Doctor.all
       flash.now[:error] = "Please choose a date for your appointment."
       render :new
     end
   end
 
+   # Prepare to select a time for the appointment
   def choose_time
+      # Calls the prepare_appointment method to set up necessary variables for appointment creation
     prepare_appointment
   end
   
-  def finalize
+   # Process the form submission for finalizing an appointment
+   def finalize
+    # Fetch the current logged-in patient
     @patient = current_patient
+    # Build a new appointment instance using parameters from the form
     @appointment = @patient.appointments.new(appointment_params)
     @appointment.patient_id = @patient.id
-    Rails.logger.info @appointment
+    # Attempt to save the appointment
     if @appointment.save!
-      # Remove the appointment_date from the session
+      # If successful, remove the appointment date from the session and display a success message
       session.delete(:appointment_date)
       flash[:success] = "Appointment was successfully created."
       redirect_to dashboard_path
     else
+      # If unsuccessful, display an error message and render the choose_time view again
       flash.now[:error] = "An error occurred while creating the appointment."
       prepare_appointment
       render :choose_time
@@ -47,39 +66,38 @@ class AppointmentsController < ApplicationController
   
   private
 
-  def prepare_appointment
-    # Fetch the doctor using the ID from params
+   # Set up necessary variables for appointment creation and time selection
+   def prepare_appointment
+    # Fetch the chosen doctor and current patient
     @doctor = Doctor.find(params[:doctor_id])
-    
-    # Fetch the patient using the ID from params
     @patient = current_patient
-    
-    # Build an appointment for the patient and doctor
+    # Build a new appointment for the current patient and chosen doctor
     @appointment = @patient.appointments.build(doctor: @doctor)
-    
-    # Fetch the date from session and convert it to a date object
+    # Parse the chosen date from the session
     @appointment_date = Date.parse(session[:appointment_date])
-    
-    # Define the full range of possible appointment times
+    # Define the range of possible appointment times
     @all_times = (9..12).to_a + (14..20).to_a
-    
-    # Fetch the times when the doctor already has appointments on the chosen date
+    # Find when the doctor already has appointments on the chosen date
     existing_appointment_times = @doctor.appointments.where(appointment_date: @appointment_date).pluck(:appointment_time).map { |time| time.hour }
-    # Filter out the times when the doctor already has appointments
+    # Determine the times when the doctor is available
     @available_times = @all_times - existing_appointment_times
   end
 
+  # Ensure the patient is logged in
   def logged_in_patient
+    # If the patient is not logged in, display an error and redirect to the login page
     unless current_patient.present?
       flash[:danger] = "You need to log in to view appointments"
       redirect_to patients_sessions_path
     end
   end
 
-  def set_patient  
+   # Fetch the current logged-in patient
+   def set_patient  
     @patient = current_patient
   end
 
+  # Define the parameters allowed for creating an appointment
   def appointment_params  
     params.require(:appointment).permit(:appointment_date, :appointment_time, :doctor_id, :patient_id)
   end
