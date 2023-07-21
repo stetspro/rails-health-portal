d9e2fc41313282158ad40d842329447c711c76c0
stetspro <stets.2033@ukr.net>
2023-07-21 01:49:10 +0000

    diagnosticform submission finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_patient,
+  helper_method :current_patient
 
   def current_patient
     @current_patient ||= Patient.find(session[:patient_id]) if session[:patient_id]
