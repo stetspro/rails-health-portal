860c3eaaaab6ae878710a35f6f284c453d6d32c5
stetspro <stets.2033@ukr.net>
2023-07-24 17:08:35 +0000

    remove expiration_date from medications, popoulate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_24_165937) do
+ActiveRecord::Schema.define(version: 2023_07_24_170714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2023_07_24_165937) do
   create_table "medications", force: :cascade do |t|
     t.string "dosage"
     t.string "frequency"
-    t.date "expiration_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "delivery"
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,31 @@
 Hospital.create(name: 'Hospital 1', address: '123 Street, City, Country')
 Hospital.create(name: 'Hospital 2', address: '456 Avenue, City, Country')
 Hospital.create(name: 'Hospital 3', address: '789 Boulevard, City, Country')
+
+
+medications = [
+  { pharmacological_name: "Acetaminophen", brand_name: "Tylenol", dosage: "500mg", frequency: "Every 6 hours", delivery: "Oral" },
+  { pharmacological_name: "Ibuprofen", brand_name: "Advil", dosage: "200mg", frequency: "Every 4 to 6 hours", delivery: "Oral" },
+  { pharmacological_name: "Amlodipine", brand_name: "Norvasc", dosage: "5mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Amoxicillin", brand_name: "Amoxil", dosage: "500mg", frequency: "Every 8 hours", delivery: "Oral" },
+  { pharmacological_name: "Atorvastatin", brand_name: "Lipitor", dosage: "20mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Cetirizine", brand_name: "Reactine", dosage: "10mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Clopidogrel", brand_name: "Plavix", dosage: "75mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Diphenhydramine", brand_name: "Benadryl", dosage: "25mg", frequency: "Every 4 to 6 hours", delivery: "Oral" },
+  { pharmacological_name: "Duloxetine", brand_name: "Cymbalta", dosage: "60mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Escitalopram", brand_name: "Cipralex", dosage: "10mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Hydrochlorothiazide", brand_name: "Hydrodiuril", dosage: "25mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Lisinopril", brand_name: "Prinivil", dosage: "10mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Metformin", brand_name: "Glucophage", dosage: "500mg", frequency: "Twice daily", delivery: "Oral" },
+  { pharmacological_name: "Metoprolol", brand_name: "Lopressor", dosage: "50mg", frequency: "Twice daily", delivery: "Oral" },
+  { pharmacological_name: "Omeprazole", brand_name: "Prilosec", dosage: "20mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Pregabalin", brand_name: "Lyrica", dosage: "75mg", frequency: "Twice daily", delivery: "Oral" },
+  { pharmacological_name: "Simvastatin", brand_name: "Zocor", dosage: "20mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Tadalafil", brand_name: "Cialis", dosage: "10mg", frequency: "As needed", delivery: "Oral" },
+  { pharmacological_name: "Venlafaxine", brand_name: "Effexor", dosage: "75mg", frequency: "Once daily", delivery: "Oral" },
+  { pharmacological_name: "Warfarin", brand_name: "Coumadin", dosage: "5mg", frequency: "Once daily", delivery: "Oral" },
+]
+
+medications.each do |medication|
+  Medication.create!(medication)
+end
