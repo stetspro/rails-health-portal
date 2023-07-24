1d96d32eeb21209484dcdff168f841aae8259ad8
stetspro <stets.2033@ukr.net>
2023-07-24 16:53:08 +0000

    add medications/index

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,10 @@
 class MedicationsController < ApplicationController
+  before_action :authenticate_patient!
+  before_action :restrict_patient!
+
+  def index
+    @patient = current_patient
+    @patient_medications = @patient.patient_medications
+  end
+
 end
