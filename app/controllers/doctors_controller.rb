752ba2e7ca6b9a17e3500e91a626605036a6ddd5
stetspro <stets.2033@ukr.net>
2023-07-28 02:08:09 +0000

    style jsCalendar

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -8,12 +8,7 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.includes(appointments: :patient).find(params[:id])
     @appointments = @doctor.appointments.where(appointment_date: Date.today).order(:appointment_time)
   end
-  
-  def current_time_in_appointment_range?(appointment)
-    current_time = Time.now
-    (appointment.appointment_time..appointment.appointment_time + 59.minutes).cover?(current_time)
-  end
-  
+
   private
   def doctor_params
     params.require(:doctor).permit(:name, :specialization, :email, :address)
--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,2 +1,6 @@
 module DoctorsHelper
+  def current_time_in_appointment_range?(appointment)
+    current_time = Time.now
+    (appointment.appointment_time..appointment.appointment_time + 59.minutes).cover?(current_time)
+  end
 end
