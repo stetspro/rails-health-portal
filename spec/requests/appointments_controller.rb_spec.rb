f5a50bfd5c39b3c5bbe07699280b7ef068cfa7e0
stetspro <stets.2033@ukr.net>
2023-11-29 00:00:00 +0000

    add get#new unit test for appointmentscontroller

--- spec/requests/appointments_controller.rb_spec.rb
+++ spec/requests/appointments_controller.rb_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "AppointmentsController.rbs", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+  describe "GET #new" do
+    before do
+      get :new
+    end
+    it "responds successfully" do
+      expect(response).to be_successful
+    end
+
+    it "assigns @appointment" do
+      expect(assigns(:appointment)).to be_a_new(Appointment)
+    end
 end
